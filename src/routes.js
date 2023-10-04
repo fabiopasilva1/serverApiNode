aba76bd4559ecf8a1f9429764ee87b1a4d83de53
fabiopasilva1 <fabiopasilva1@gmail.com>
2023-10-04 17:34:30 -0300

    Fix the variables for querying the database

--- src/routes.js
+++ src/routes.js
@@ -26,31 +26,36 @@ function verifyToken(req, next) {
 }
 
 router.get("/api/equipamentos", (req, res, next) => {
-	const verify = verifyToken(req, next);
-	const table = req.query.table;
-	const where = req.query.filter && `WHERE ${req.query.filter}`;
-	const order = req.query.order && req.query.order.split(":");
-	const orderBy = order[0];
-	const orderDirection = order[1];
-	const OrderByDirection = ` ORDER BY ${orderBy} ${orderDirection.toUpperCase()}`;
+	if (req) {
+		const verify = verifyToken(req, next);
+		const table = req.query.table;
+		const where = req.query.filter && `WHERE ${req.query.filter}`;
+		const order = req.query.order && req.query.order.split(":");
+		const orderBy = order && order[0];
+		const orderDirection = order && order[1];
+		const OrderByDirection =
+			(orderDirection &&
+				` ORDER BY ${orderBy} ${orderDirection.toUpperCase()}`) ||
+			"";
 
-	if (verify) {
-		pool.query(
-			"SELECT * FROM " + table + " pv " + where + "" + OrderByDirection + "",
-			(err, results) => {
-				if (err) {
-					console.error("Erro ao executar a consulta:", err);
-					res.status(500).send("Erro ao consultar o banco de dados");
-				} else {
-					res.json(results);
+		if (verify) {
+			pool.query(
+				"SELECT * FROM " + table + " pv " + where + "" + OrderByDirection + "",
+				(err, results) => {
+					if (err) {
+						console.error("Erro ao executar a consulta:", err);
+						res.status(500).send("Erro ao consultar o banco de dados");
+					} else {
+						res.json(results);
+					}
 				}
-			}
-		);
-	} else {
-		res.json({
-			error: "Acesso não permitido, credenciais inválidads",
-			status: 401,
-		});
+			);
+		} else {
+			res.json({
+				error: "Acesso não permitido, credenciais inválidads",
+				status: 401,
+			});
+		}
 	}
 });
 const SECRET_KEY = process.env.SERVER_SECRET_KEY;
