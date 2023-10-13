e30c388da9ea13e4132767dbbd45f2aa2540e723
fabiopasilva1 <fabiopasilva1@gmail.com>
2023-10-13 20:08:43 -0300

    Updates DB

--- main/db.js
+++ main/db.js
@@ -20,7 +20,7 @@ function createPostgresPool() {
 }
 
 function createMysqlPool() {
-	const connection = mysql.createConnection({
+	return mysql.createConnection({
 		user: process.env.DB_USERNAME,
 		host: process.env.DB_HOST,
 		database: process.env.DB_NAME,
@@ -30,41 +30,6 @@ function createMysqlPool() {
 		connectionLimit: 10,
 		queueLimit: 0,
 	});
-
-	// Adiciona um ouvinte de evento de erro para lidar com desconexões
-	connection.on("error", (err) => {
-		console.error("Erro na conexão MySQL:", err);
-		if (
-			err.code === "PROTOCOL_CONNECTION_LOST" ||
-			err.code === "ETIMEDOUT" ||
-			err.code === "ECONNRESET" ||
-			err.code === "EHOSTUNREACH"
-		) {
-			console.log("Tentando reconectar ao ", driver);
-			// Reconectar em caso de perda de conexão
-			setTimeout(() => {
-				pool = createMysqlPool();
-			}, 3000);
-		} else {
-			throw err;
-		}
-	});
-	connection.on("connect", () => {
-		console.log("Conexão inicial estabelecida com sucesso! driver:", driver);
-	});
-
-	return connection;
 }
 
-if (driver === "postgres") {
-	console.log("Conectando... ", driver);
-	pool = createPostgresPool();
-} else if (driver === "mysql") {
-	console.log("Conectando...", driver);
-	pool = createMysqlPool();
-} else {
-	console.error("Tipo de banco de dados não suportado:", driver);
-	process.exit(1);
-}
-
-module.exports = pool;
+module.exports = { createPostgresPool, createMysqlPool, driver };
--- src/routes/apiRoute.js
+++ src/routes/apiRoute.js
@@ -1,18 +1,57 @@
 const express = require("express");
 const { authenticateJWT } = require("../../main/middlewares");
 const cors = require("cors");
-var pool = require("../../main/db");
+var { driver, createPostgresPool, createMysqlPool } = require("../../main/db");
 const bodyParser = require("body-parser");
 const apiRoute = express.Router();
-
+console.log(driver);
 apiRoute.use(cors());
 apiRoute.use(authenticateJWT);
 apiRoute.get("/", (req, res) => {
 	res.json("Bem vindo ao servidor Api Node");
 });
-
+let pool;
 apiRoute.get("/equipamentos", (req, res, next) => {
 	if (req) {
+		if (driver === "postgres") {
+			console.log("Conectando... ", driver);
+			pool = createPostgresPool();
+		} else if (driver === "mysql") {
+			console.log("Conectando...", driver);
+			pool = createMysqlPool();
+		} else {
+			console.error("Tipo de banco de dados não suportado:", driver, error);
+			process.exit(1);
+		}
+		// Adiciona um ouvinte de evento de erro para lidar com desconexões
+		pool.on("error", (err) => {
+			console.error("Erro na conexão MySQL:", err);
+			if (
+				err.code === "PROTOCOL_CONNECTION_LOST" ||
+				err.code === "ETIMEDOUT" ||
+				err.code === "ECONNRESET" ||
+				err.code === "EHOSTUNREACH"
+			) {
+				console.log("Tentando reconectar ao ", driver);
+				// Reconectar em caso de perda de conexão
+				setTimeout(() => {
+					pool = createMysqlPool(); // Atualize a variável pool aqui
+				}, 3000);
+			} else {
+				throw err;
+			}
+		});
+		pool.on("connect", () => {
+			console.log("Conexão inicial estabelecida com sucesso! driver:", driver);
+		});
+		if (pool) {
+			pool.on("connect", () => {
+				console.log(
+					"Conexão inicial estabelecida com sucesso! driver:",
+					driver
+				);
+			});
+		}
 		const table = req.query.table;
 		const where = req.query.filter && `WHERE ${req.query.filter}`;
 		const order = req.query.order && req.query.order.split(":");
@@ -23,16 +62,81 @@ apiRoute.get("/equipamentos", (req, res, next) => {
 				` ORDER BY ${orderBy} ${orderDirection.toUpperCase()}`) ||
 			"";
 
-		pool
-			.promise()
-			.query(
-				"SELECT * FROM " + table + " pv " + where + "" + OrderByDirection + ""
-			)
-			.then(([rows, fields]) => {
-				res.json(rows);
-			})
-			.catch(console.log);
+		pool &&
+			pool
+				.promise()
+				.query(
+					"SELECT * FROM " + table + " pv " + where + "" + OrderByDirection + ""
+				)
+				.then(([rows, fields]) => {
+					res.json(rows);
+				})
+				.catch(console.log);
 	}
 });
 
+apiRoute.get("/historico", (req, res, next) => {
+	if (req) {
+		if (driver === "postgres") {
+			console.log("Conectando... ", driver);
+			pool = createPostgresPool();
+		} else if (driver === "mysql") {
+			console.log("Conectando...", driver);
+			pool = createMysqlPool();
+		} else {
+			console.error("Tipo de banco de dados não suportado:", driver, error);
+			process.exit(1);
+		}
+		// Adiciona um ouvinte de evento de erro para lidar com desconexões
+		pool.on("error", (err) => {
+			console.error("Erro na conexão MySQL:", err);
+			if (
+				err.code === "PROTOCOL_CONNECTION_LOST" ||
+				err.code === "ETIMEDOUT" ||
+				err.code === "ECONNRESET" ||
+				err.code === "EHOSTUNREACH"
+			) {
+				console.log("Tentando reconectar ao ", driver);
+				// Reconectar em caso de perda de conexão
+				setTimeout(() => {
+					pool = createMysqlPool(); // Atualize a variável pool aqui
+				}, 3000);
+			} else {
+				throw err;
+			}
+		});
+		pool.on("connect", () => {
+			console.log("Conexão inicial estabelecida com sucesso! driver:", driver);
+		});
+		if (pool) {
+			pool.on("connect", () => {
+				console.log(
+					"Conexão inicial estabelecida com sucesso! driver:",
+					driver
+				);
+			});
+		}
+		const table = req.query.table;
+		console.log(table);
+		const where = (req.query.filter && `WHERE ${req.query.filter}`) || "";
+		const order = req.query.order && req.query.order.split(":");
+		const orderBy = order && order[0];
+		const orderDirection = order && order[1];
+		const OrderByDirection =
+			(orderDirection &&
+				` ORDER BY ${orderBy} ${orderDirection.toUpperCase()}`) ||
+			"";
+
+		pool &&
+			pool
+				.promise()
+				.query(
+					"SELECT * FROM " + table + " " + where + "" + OrderByDirection + ""
+				)
+				.then(([rows, fields]) => {
+					res.json(rows);
+				})
+				.catch(console.log);
+	}
+});
 module.exports = apiRoute;
